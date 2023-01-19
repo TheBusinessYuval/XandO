0e36a141a86f5c2a9de573bc69864b69755ba521
TheBusinessYuval <beckbarkai@gmail.com>
2023-01-19 08:42:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/xando/chengeSize.java
@@ -0,0 +1,14 @@
+package com.example.xando;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chengeSize extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chenge_size);
+    }
+}
